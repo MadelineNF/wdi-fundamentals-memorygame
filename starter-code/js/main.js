e86a0de530254f25b22ae83d2fbb3eac22347984
MadelineNF <madelinenelsonfolkersen@gmail.com>
2017-03-22 15:15:53 -0400

    created card divs with JS

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,12 +1,20 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
-var cardOne = "Queen";
-cardTwo = "King";
-cardThree = "Queen";
-cardFour = "King";
+var cardOne = "Queen"; cardTwo = "King"; cardThree = "Queen"; cardFour = "King";
+var x;
+var cardFun;
+cardFun = function () {
+    for (var i = 0; i < 4; i++){
+       var creatEl = document.createElement("div");
+        creatEl.className = "card";
+        document.querySelector('#game-board').appendChild(creatEl);
+    }
+};
+x = document.getElementById("game-board");
 
-if (cardFour == cardTwo) {
+cardFun();
+/*if (cardFour == cardTwo) {
     window.alert("You found a match!");
 } else{
     window.alert("Try again");
-}
+}*/
