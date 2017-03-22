94d614202e32998eb74908f3e62f0968c0fc0002
MadelineNF <madelinenelsonfolkersen@gmail.com>
2017-03-22 12:50:05 -0400

    added more game logic!

---
+++ starter-code/js/main.js
@@ -0,0 +1,12 @@
+console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "Queen";
+cardTwo = "King";
+cardThree = "Queen";
+cardFour = "King";
+
+if (cardFour == cardTwo) {
+    window.alert("You found a match!");
+} else{
+    window.alert("Try again");
+}
--- starter-code/main.js
+++
@@ -1,2 +0,0 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = "Queen"; cardTwo = "King"; cardThree = "Queen"; cardFour = "King";
